8579ac6d3a8eae78bb907fbfce88e00293c48acf
Nagy Zsolt <stay.pista@gmail.com>
2017-03-21 21:44:14 +0100

    Feladatkiírás

---
+++ Gyakorlofeladat-03-20/main.c
@@ -0,0 +1,69 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**Programozás korrepetálás második heti házifeladat.
+ * Téma: Bitmaszkok, ciklusok, beolvasás, kiírás, függvények
+ * Kérem a teljesítési időket rögzítse!
+ * A feladatokat a kiírás alá végezze el.
+ * Célidő: 19 perc + 10 perc(bónusz)
+ * Superuser idő: 5 perc
+ */
+
+int main()
+{
+    printf("*** 1. feladat ***\n");
+    /** 1. Feladat:
+     *  Hozzon létre egy ciklust mely 100 tól számol vissza egészen 0ig! (0 is beletartozik)
+     *  Az egyes értékeken szóközzel elválasztva írja ki!
+     *  Célidő: 3 perc
+     *  Teljesítési idő:
+     */
+
+
+    printf("\n\n*** 2. feladat ***\n");
+    /** 2. Feladat:
+     *  Hozzon létre egy ciklust mely 100 tól számol egészen 500-ig! (500 nem tartozik bele)
+     *  Írja ki azokat az értékeket melyek párosak és oszthatóak 3-al maradék nélkül!
+     *  Célidő: 3 perc
+     *  Teljesítési idő:
+     */
+
+
+    printf("\n\n*** 3. feladat ***\n");
+    /** 3. Feladat:
+     *  Hozzon létre egy ciklust mely 750 től számol egészen 800-ig! (750 nem tartozik bele)
+     *  Írja ki azokat az értékeket melyek párosak, csak bitmaszkokkal!
+     *  Célidő: 3 perc
+     *  Teljesítési idő:
+     */
+
+
+    printf("\n\n*** 4. feladat ***\n");
+    /** 4. Feladat:
+     *  Hozzon létre egy ciklust mely 90 től számol egészen 765-ig!
+     *  Írja ki azokat az értékeket melyek 5. és 2. bitje '1' és 0. bitje '0'!
+     *  Célidő: 5 perc
+     *  Teljesítési idő:
+     */
+
+
+    printf("\n\n*** 5. feladat ***\n");
+    /** 5. Feladat:
+     *  Scanf függvénynel kérjen be egy stringet majd számolja meg és írja ki
+     *  hogy hány karakter szerepel a beolvasáshoz használt tömben.
+     *  Célidő: 5 perc
+     *  Teljesítési idő:
+     */
+
+
+    printf("\n\n*** Bonusz. feladat ***\n");
+    /** Bónusz Feladat:
+     *  Készítsen egy függvényt mely a bemenetében vár egy értéket és a visszatérési értékével
+     *  a bevitt érték faktoriálisát adja vissza!
+     *  Célidő: infinit perc
+     *  Teljesítési idő:
+     */
+
+
+    return 0;
+}
